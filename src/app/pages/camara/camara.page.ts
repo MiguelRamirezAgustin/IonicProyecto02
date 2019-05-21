990d941ca68c317b28c32bdc79b211a8a19ad5e0
MiguelRamirezAgustin <miguel.r.agustin@gmail.com>
2019-05-20 19:40:39 -0500

    camara ios y android

--- src/app/pages/camara/camara.page.ts
+++ src/app/pages/camara/camara.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-camara',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CamaraPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private camera:Camera) { }
+  currentImage: string;
+  image:string= null;
+  
   ngOnInit() {
+  }
+
+
+  openCam(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      // saveToPhotoAlbum: false,
+      // correctOrientation: true
+    }
+    this.camera.getPicture(options).then((imageData) => {
+      //this.image =  imageData;
+     this.image=(<any>window).Ionic.WebView.convertFileSrc(imageData);
+    }, (err) => {
+     alert("error "+JSON.stringify(err))
+    });
   }
 
 }
